966830ed642f5f340dde8db8511b676f7f6f892a
isht3 <4darshofficial@gmail.com>
2018-05-13 19:36:54 +0530

    Add token through token file

--- evalai/auth.py
+++ evalai/auth.py
@@ -2,17 +2,8 @@ import click
 
 from click import echo
 
-from evalai.utils.auth import save_token
 
 @click.command()
-@click.option('--token', '-t',
-              default='',
-              help='Pass token obtained from your EvalAI account.')
-def auth(token):
-    """
-    Connect your EvalAI account with the CLI.
-    """
-    if (token == ''):
-        echo("Please pass a Token. Use evalai auth --help for help.")
-    else:
-        save_token(token)
+def auth():
+    """Example script."""
+    echo('Hello auth!')
--- evalai/utils/auth.py
+++ evalai/utils/auth.py
@@ -1,35 +1,22 @@
-import os
-import requests
+import json
+from pathlib import Path
 
 from click import echo
 
 
-CONFIG_FILE = '.config'
-__location__ = os.path.realpath(os.path.join(os.getcwd(),
-                                    os.path.dirname(__file__)))
-FULL_PATH = os.path.join(__location__, CONFIG_FILE)
-
-
-def save_token(token):
-    """
-    Saves the token in a config file.
-    """
-    with open(FULL_PATH, 'w') as fw:
-        try:
-            fw.write(token)
-            echo('Your token has been saved.')
-        except (OSError, IOError) as e:
-            echo(e)
+CONFIG_FILE = 'token.json'
+FULL_PATH = str(Path.home()) + "/.evalai/{}".format(CONFIG_FILE)
 
 
 def load_token():
     """
     Loads token to be used for sending requests.
     """
-    token = ''
     with open(FULL_PATH, 'r') as fr:
         try:
-            token = fr.read()
+            data = fr.read()
         except (OSError, IOError) as e:
             echo(e)
+    data = json.loads(data)
+    token = data["token"]
     return token
