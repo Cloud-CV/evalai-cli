c583cafb9c8e9e07601063f3dc8745fd5a389e5b
Rishabh Jain <rishabhjain2018@gmail.com>
2019-02-11 15:11:21 -0500

    Update login.py

--- evalai/login.py
+++ evalai/login.py
@@ -9,11 +9,11 @@ from evalai.utils.config import AUTH_TOKEN_PATH, AUTH_TOKEN_DIR
 
 @click.group(invoke_without_command=True)
 @click.pass_context
-@click.argument("USERNAME", type=str)
-def login(ctx, username):
+def login(ctx):
     """
     Login to EvalAI and save token.
     """
+    username = click.prompt("username", type=str, hide_input=False)
     password = click.prompt("Enter password", type=str, hide_input=True)
     token = get_user_auth_token_by_login(username, password)
 
