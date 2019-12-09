53613ef8e2430c08a71184ce180ae08fdbd66138
RohanSreelesh <31416057+RohanSreelesh@users.noreply.github.com>
2019-12-09 19:44:34 +0530

    Update test_auth.py

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -200,7 +200,13 @@ class TestSetAndLoadHostURL(BaseTestClass):
             )
             end_date = convert_UTC_date_to_local(challenge_json["end_date"])
             values.extend([creator, start_date, end_date])
-            table.append_row(values)
+            table.append_row([colored(values[0], 'white'),
+                              colored(values[1], 'yellow'),
+                              colored(values[2], 'cyan'),
+                              colored(values[3], 'white'),
+                              colored(values[4], 'green'),
+                              colored(values[5], 'red'),
+                              ])
         self.output = str(table)
 
     def teardown(self):
@@ -213,4 +219,4 @@ class TestSetAndLoadHostURL(BaseTestClass):
         result = runner.invoke(host, ["-sh", "https://evalapi.cloudcv.org"])
         result = runner.invoke(challenges)
         response = result.output.strip()
-        assert response == self.output
+        assert str(response) == self.output
