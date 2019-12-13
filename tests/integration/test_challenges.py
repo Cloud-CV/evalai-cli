c16b09a74ea5ad81830a9422ecf2aa1a014bfd82
nikochiko <kaustubh299792458@gmail.com>
2019-12-14 01:38:16 +0530

    Update

--- tests/integration/test_challenges.py
+++ tests/integration/test_challenges.py
@@ -1,6 +1,10 @@
 import requests
 
+from click.testing import CliRunner
 from unittest import TestCase
+
+from evalai.add_token import set_token
+from evalai.challenges import challenges, challenge
 
 from evalai.utils.config import API_HOST_URL
 from evalai.utils.urls import URLS
@@ -11,10 +15,13 @@ class BaseTestClass(TestCase):
         self.url = "{}{}"
         self.host_url = API_HOST_URL
 
+    def set_token_to_undefined(self):
+        runner = CliRunner()
+        runner.invoke(set_token, "0"*40)
+
     def test_challenges_when_token_is_missing(self):
-        response = requests.get(
-            self.url.format(self.host_url, URLS.challenge_list.value)
-        )
-        expected = "Sorry, no challenges found."
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.content, expected)
+        self.set_token_to_undefined()
+        runner = CliRunner()
+        expected = "The authentication token you are using isn't valid. Please generate it again."
+        result = runner.invoke(challenges)
+        assert expected == result.rstrip()
