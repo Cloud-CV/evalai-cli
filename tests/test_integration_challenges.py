fd694ba02caacb7c73e9fcd0d8114a83197617f6
nikochiko <kaustubh299792458@gmail.com>
2019-12-14 16:48:21 +0530

    some changes

--- tests/test_integration_challenges.py
+++ tests/test_integration_challenges.py
@@ -1,27 +1,27 @@
 from click.testing import CliRunner
 
 from evalai.add_token import set_token
+from evalai.login import login
 from evalai.challenges import challenges, challenge
 
 
-class BaseTestClass:
+class TestIntegrationChallenges:
     def setUp(self):
         self.url = "{}{}"
         self.undefined_token = "0" * 40
-        self.valid_testuser_token = "3c6dcbdb50b6edc2942f4629c0c1ca51fa80d88c"
 
         # Temporary Solution, a long term solution will have to be more dynamic
         # and adaptables
-        self.challenge_data_string = """
-        +-----+--------------------+------------------------------------------------------------------------------------------------------------------+----------+----------------------+----------------------+\n
-        | ID  |       Title        |                                                Short Description                                                 | Creator  |      Start Date      |       End Date       |\n
-        +-----+--------------------+------------------------------------------------------------------------------------------------------------------+----------+----------------------+----------------------+\n
-        | 163 | VQA Challenge 2019 | Recent progress in computer vision and natural language processing has demonstrated that lower-level tasks are m | VQA Team | 01/29/19 05:29:59 AM | 01/01/00 05:29:59 AM |\n
-        |     |                    | uch closer to being solved. We believe that the time is ripe to pursue higher-level tasks, one of which is Visua |          |                      |                      |\n
-        |     |                    | l Question Answering (VQA), where the goal is to be able to understand the semantics of scenes well enough to be |          |                      |                      |\n
-        |     |                    |          able to answer open-ended, free-form natural language questions (asked by humans) about images.         |          |                      |                      |\n
-        +-----+--------------------+------------------------------------------------------------------------------------------------------------------+----------+----------------------+----------------------+\n
-        """
+        # self.challenge_data_string = """
+        # +-----+--------------------+------------------------------------------------------------------------------------------------------------------+----------+----------------------+----------------------+\n
+        # | ID  |       Title        |                                                Short Description                                                 | Creator  |      Start Date      |       End Date       |\n
+        # +-----+--------------------+------------------------------------------------------------------------------------------------------------------+----------+----------------------+----------------------+\n
+        # | 163 | VQA Challenge 2019 | Recent progress in computer vision and natural language processing has demonstrated that lower-level tasks are m | VQA Team | 01/29/19 05:29:59 AM | 01/01/00 05:29:59 AM |\n
+        # |     |                    | uch closer to being solved. We believe that the time is ripe to pursue higher-level tasks, one of which is Visua |          |                      |                      |\n
+        # |     |                    | l Question Answering (VQA), where the goal is to be able to understand the semantics of scenes well enough to be |          |                      |                      |\n
+        # |     |                    |          able to answer open-ended, free-form natural language questions (asked by humans) about images.         |          |                      |                      |\n
+        # +-----+--------------------+------------------------------------------------------------------------------------------------------------------+----------+----------------------+----------------------+\n
+        # """
 
     def set_token_to(self, token):
         runner = CliRunner()
@@ -29,6 +29,10 @@ class BaseTestClass:
 
     def set_token_to_undefined(self):
         self.set_token_to(self.undefined_token)
+
+    def login_as_participant(self):
+        runner = CliRunner()
+        runner.invoke(login, ["participant", "password"])
 
     def test_challenges_when_token_is_invalid(self):
         self.set_token_to_undefined()
@@ -38,7 +42,7 @@ class BaseTestClass:
         assert expected == result.output
 
     def test_challenge_details_when_challenge_id_is_not_int(self):
-        self.set_token_to(self.valid_testuser_token)
+        self.login_as_participant()
         runner = CliRunner()
         expected = "{}{}".format(
             "Usage: evalai challenge [OPTIONS] CHALLENGE COMMAND [ARGS]...\n",
@@ -47,10 +51,10 @@ class BaseTestClass:
         result = runner.invoke(challenge, "not_integer")
         assert expected == result.output
 
-    def test_display_challenges_participated(self):
-        self.set_token_to(self.valid_testuser_token)
-
-        runner = CliRunner()
-        expected = "\n{}\n{}\n".format("Participated Challenges", self.challenge_data_string)
-        result = runner.invoke(challenges, ["--participant"])
-        assert expected == result.output
+    # def test_display_challenges_participated(self):
+    #     self.set_token_to(self.valid_testuser_token)
+    #
+    #     runner = CliRunner()
+    #     expected = "\n{}\n{}\n".format("Participated Challenges", self.challenge_data_string)
+    #     result = runner.invoke(challenges, ["--participant"])
+    #     assert expected == result.output
