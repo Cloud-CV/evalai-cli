2d2e7a520625416d79c273f83bfbe1663939d06b
nikochiko <kaustubh299792458@gmail.com>
2019-12-15 16:00:34 +0530

    Add integration tests

--- evalai/login.py
+++ evalai/login.py
@@ -8,13 +8,13 @@ from evalai.utils.config import AUTH_TOKEN_PATH, AUTH_TOKEN_DIR
 
 
 @click.group(invoke_without_command=True)
-@click.argument("--username", prompt=True)
-@click.argument("--passowrd", prompt=True)
 @click.pass_context
 def login(ctx, username, password):
     """
     Login to EvalAI and save token.
     """
+    username = click.prompt("username", type=str, hide_input=False)
+    password = click.prompt("Enter password", type=str, hide_input=True)
     token = get_user_auth_token_by_login(username, password)
 
     if os.path.exists(AUTH_TOKEN_PATH):
---
+++ tests/integration/test_integration_challenges.py
@@ -0,0 +1,37 @@
+from click.testing import CliRunner
+
+from evalai.add_token import set_token
+from evalai.login import login
+from evalai.challenges import challenges, challenge
+
+
+class TestIntegrationChallenges:
+    def setUp(self):
+        self.url = "{}{}"
+        self.undefined_token = "0" * 40
+        self.valid_testuser_token = "3c6dcbdb50b6edc2942f4629c0c1ca51fa80d88c"
+        self.set_token_to(self.valid_testuser_token)
+
+    def set_token_to(self, token):
+        runner = CliRunner()
+        runner.invoke(set_token, token)
+
+    def set_token_to_undefined(self):
+        self.set_token_to(self.undefined_token)
+
+    # Temporary Tests
+    def test_challenges_when_token_is_invalid(self):
+        self.set_token_to_undefined()
+        runner = CliRunner()
+        expected = "\nThe authentication token you are using isn't valid. Please generate it again.\n\n"
+        result = runner.invoke(challenges)
+        assert expected == result.output
+
+    def test_challenge_details_when_challenge_id_is_not_int(self):
+        runner = CliRunner()
+        expected = "{}{}".format(
+            "Usage: challenge [OPTIONS] CHALLENGE COMMAND [ARGS]...\n",
+            "Error: Invalid value for \"CHALLENGE\": x is not a valid integer\n"
+        )
+        result = runner.invoke(challenge, "x")
+        assert expected == result.output
