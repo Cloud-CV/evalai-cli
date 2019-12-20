5fa15988b967b553223cd8072655234abf5c1e34
nikochiko <kaustubh299792458@gmail.com>
2019-12-20 12:13:24 +0530

    Fix mock syntax

--- tests/test_auth_utils.py
+++ tests/test_auth_utils.py
@@ -19,7 +19,7 @@ class TestWriteHostUrlToFile(TestCase):
         with mock.patch('sys.stdout', new=StringIO()) as fake_out:
             write_host_url_to_file(self.new_host)
             mock_open.assert_called_with(HOST_URL_FILE_PATH, "w")
-            mock_open.write.assert_called_with(self.new_host)
+            mock_open().write.assert_called_with(self.new_host)
             self.assertEqual(fake_out.get_value(), expected)
         patcher.stop()
 
@@ -27,7 +27,7 @@ class TestWriteHostUrlToFile(TestCase):
         mock_open = mock.mock_open()
         patcher = mock.patch("evalai.utils.auth.open", mock_open)
         patcher.start()
-        mock_open.write.side_effect = OSError("Permission denied.")
+        mock_open().write.side_effect = OSError("Permission denied.")
         expected = "Permission denied.\nNone\n"
         with mock.patch('sys.stdout', new=StringIO()) as fake_out:
             with self.assertRaises(SystemExit) as cm:
