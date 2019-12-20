a1b1e5e407e89b0885fa53a3e69631f5a547983b
nikochiko <kaustubh299792458@gmail.com>
2019-12-20 12:23:03 +0530

    Troubleshoot

--- tests/test_auth_utils.py
+++ tests/test_auth_utils.py
@@ -12,27 +12,29 @@ class TestWriteHostUrlToFile(TestCase):
         self.new_host = "http://testserver.abc"
 
     def test_write_host_url_to_file_success(self):
-        mock_open = mock.mock_open()
-        patcher = mock.patch("evalai.utils.auth.open", mock_open)
+        fake_open = mock.mock_open()
+        patcher = mock.patch("evalai.utils.auth.open", fake_open)
         patcher.start()
+        handler = fake_open()
         expected = "{} is set as the host url.\n".format(self.new_host)
         with mock.patch('sys.stdout', new=StringIO()) as fake_out:
             write_host_url_to_file(self.new_host)
-            mock_open.assert_called_with(HOST_URL_FILE_PATH, "w")
-            mock_open().write.assert_called_with(self.new_host)
-            self.assertEqual(fake_out.get_value(), expected)
+            fake_open.assert_called_with(HOST_URL_FILE_PATH, "w")
+            handler.write.assert_called_with(self.new_host)
+            self.assertEqual(fake_out.getvalue(), expected)
         patcher.stop()
 
     def test_write_host_url_to_file_fail(self):
-        mock_open = mock.mock_open()
-        patcher = mock.patch("evalai.utils.auth.open", mock_open)
+        fake_open = mock.mock_open()
+        patcher = mock.patch("evalai.utils.auth.open", fake_open)
         patcher.start()
-        mock_open().write.side_effect = OSError("Permission denied.")
+        handler = fake_open()
+        handler.write.side_effect = OSError("Permission denied.")
         expected = "Permission denied.\nNone\n"
         with mock.patch('sys.stdout', new=StringIO()) as fake_out:
             with self.assertRaises(SystemExit) as cm:
                 write_host_url_to_file(self.new_host)
                 self.assertEqual(cm.exception.error_code, 1)
-            mock_open.write.assert_called_with(self.new_host)
-            self.assertEqual(fake_out.get_value(), expected)
+            handler.write.assert_called_with(self.new_host)
+            self.assertEqual(fake_out.getvalue(), expected)
         patcher.stop()
