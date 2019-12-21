736f11c25ed12787d252ad787e0bf2b0ee703cf2
nikochiko <kaustubh299792458@gmail.com>
2019-12-21 12:11:59 +0530

    Fix order of login prompts, error handling

--- evalai/login.py
+++ evalai/login.py
@@ -5,10 +5,10 @@ from evalai.utils.auth import get_user_auth_token_by_login, write_json_auth_toke
 
 
 @click.group(invoke_without_command=True)
-@click.option('-p', '--password', type=str, hide_input=True, prompt=True)
 @click.option('-u', '--username', type=str, hide_input=False, prompt=True)
+@click.option('-p', '--password', type=str, hide_input=True, prompt=True)
 @click.pass_context
-def login(ctx, username, password):
+def login(ctx, password, username):
     """
     Login to EvalAI and save token.
     """
