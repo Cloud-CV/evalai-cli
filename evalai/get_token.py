ba422b3b448f8d3849779ae0d112888f5e5e41b2
Akshat <33050725+aaaakshat@users.noreply.github.com>
2019-12-24 16:33:09 +0800

    updated error messages

--- evalai/get_token.py
+++ evalai/get_token.py
@@ -31,7 +31,6 @@ def get_token():
             except (OSError, IOError) as e:
                 echo(str(e))
             except JSONDecodeError as e:
-                msg = "Decode Error: " + str(e) + "\nThe 'token.json' file was empty"
-                echo(msg)
+                echo("Token file was found empty, Please set the token using the command evalai set_token <token>")
             except KeyError as e:
-                echo("The following key was not found in 'token.json': " + str(e))
+                echo("Invalid token found, please reset the token using the command evalai set_token <token>")
