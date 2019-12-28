45cebc9286ce203d97935b17af08e3417f39adae
nikochiko <kaustubh299792458@gmail.com>
2019-12-28 14:07:00 +0530

    Add docstring for logout command

--- evalai/logout.py
+++ evalai/logout.py
@@ -7,5 +7,11 @@ from evalai.utils.auth import reset_user_auth_token
 
 @click.group(invoke_without_command=True)
 def logout():
+    """
+    Log out the user by resetting authentication token
+    """
+    """
+    Invoked by `evalai logout`
+    """
     reset_user_auth_token()
     echo(style("Logout successful", bold=True, fg="green"))
