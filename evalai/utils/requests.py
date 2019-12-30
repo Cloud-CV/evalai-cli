562eab5a52455064b97be57f08b3e242c4b81acf
nikochiko <kaustubh299792458@gmail.com>
2019-12-30 11:32:28 +0530

    Replace conditional ladder

--- evalai/utils/requests.py
+++ evalai/utils/requests.py
@@ -13,73 +13,46 @@ from .auth import get_request_header, get_host_url
 def make_request(path, method, files=None, data=None):
     url = "{}{}".format(get_host_url(), path)
     headers = get_request_header()
-
-    if method == "GET":
-        try:
-            response = requests.get(url, headers=headers)
-            response.raise_for_status()
-        except requests.exceptions.HTTPError as err:
-            if response.status_code in EVALAI_ERROR_CODES:
-                validate_token(response.json())
-                echo(
-                    style(
-                        "\nError: {}\n".format(response.json().get("error")),
-                        fg="red",
-                        bold=True,
-                    )
-                )
-            else:
-                echo(err)
-            sys.exit(1)
-        except requests.exceptions.RequestException:
-            echo(
-                style(
-                    "\nCould not establish a connection to EvalAI."
-                    " Please check the Host URL.\n",
-                    bold=True,
-                    fg="red",
-                )
-            )
-            sys.exit(1)
-        return response.json()
-    elif method == "POST":
-        if files:
-            files = {"input_file": open(files, "rb")}
-        else:
-            files = None
+    if method == "POST":
+        files = {"input_file": open(files, "rb")} if files else None
         data = {"status": "submitting"}
-        try:
-            response = requests.post(
-                url, headers=headers, files=files, data=data
-            )
-            response.raise_for_status()
-        except requests.exceptions.HTTPError as err:
-            if response.status_code in EVALAI_ERROR_CODES:
-                validate_token(response.json())
-                echo(
-                    style(
-                        "\nError: {}\n"
-                        "\nUse `evalai challenges` to fetch the active challenges.\n"
-                        "\nUse `evalai challenge CHALLENGE phases` to fetch the "
-                        "active phases.\n".format(response.json()["error"]),
-                        fg="red",
-                        bold=True,
-                    )
-                )
-            else:
-                echo(err)
-            sys.exit(1)
-        except requests.exceptions.RequestException:
+
+    try:
+        response = requests.request(method, url, data=data, files=files)
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        if response.status_code in EVALAI_ERROR_CODES:
+            validate_token(response.json())
             echo(
                 style(
-                    "\nCould not establish a connection to EvalAI."
-                    " Please check the Host URL.\n",
-                    bold=True,
+                    "\nError: {}\n".format(response.json().get("error")),
                     fg="red",
+                    bold=True,
                 )
             )
-            sys.exit(1)
-        response = json.loads(response.text)
+            echo(
+                style(
+                    "Use `evalai challenges` to fetch the active challenges."
+                    "Use `evalai challenge CHALLENGE phases` to fetch the "
+                    "active phases.",
+                    bold=True,
+                )
+            )
+        else:
+            echo(e)
+        sys.exit(1)
+    except requests.exceptions.RequestException:
+        echo(
+            style(
+                "\nCould not establish a connection to EvalAI."
+                " Please check the Host URL.\n",
+                bold=True,
+                fg="red",
+            )
+        )
+        sys.exit(1)
+
+    if method == "POST":
         echo(
             style(
                 "\nYour docker file is successfully submitted.\n",
@@ -93,16 +66,9 @@ def make_request(path, method, files=None, data=None):
                     response.get("id")
                 ),
                 bold=True,
-                fg="white"
             )
         )
-        return response
-    elif method == "PUT":
-        # TODO: Add support for PUT request
-        pass
-    elif method == "PATCH":
-        # TODO: Add support for PATCH request
-        pass
-    elif method == "DELETE":
-        # TODO: Add support for DELETE request
-        pass
+    # TODO: Add support for PUT request
+    # TODO: Add support for PATCH request
+    # TODO: Add support for DELETE request
+    return response.json()
