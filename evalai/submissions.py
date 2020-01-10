328ceb174eca6ebb97da5b02921ad3654b1fddca
Takitsuse Nagisa <takitsuse.nagisa@gmail.com>
2020-01-11 03:09:52 +0900

    Reorder imports

--- evalai/submissions.py
+++ evalai/submissions.py
@@ -17,10 +17,10 @@ from click import echo, style
 from evalai.utils.common import notify_user
 from evalai.utils.requests import make_request
 from evalai.utils.submissions import (
+    convert_bytes_to,
     display_submission_details,
     display_submission_result,
     display_submission_stderr,
-    convert_bytes_to,
 )
 from evalai.utils.urls import URLS
 from evalai.utils.config import (
