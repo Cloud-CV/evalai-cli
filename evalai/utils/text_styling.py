edc766ad2da97788e59104324f6dc15494dc33d6
ShubhamPandey28 <b18194@students.iitmandi.ac.in>
2020-01-11 22:51:53 +0530

    Fixed flake8 build error in text_styling.py

--- evalai/utils/text_styling.py
+++ evalai/utils/text_styling.py
@@ -1,30 +1,27 @@
 
-import click
 from click import echo, style
 
 
 def print_message(message, error=False):
-    
+
     style_message = ""
-    
+
     index=0
-    
+
     for i in list(message.split("`")):
-        
+
         if index%2:
-            style_message += style(i,fg="cyan")
-        
+            style_message += style(i, fg="cyan")
+
         elif error:
-            style_message += style(i,fg="red",bold=True)
-        
+            style_message += style(i, fg="red", bold=True)
+
         else:
-            style_message += style(i,fg="green")
-        
+            style_message += style(i, fg="green")
+
         index += 1
 
     echo(
         style_message,
         color=True
     )
-
-
