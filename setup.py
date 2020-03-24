6789156371592fc727b079995304e69bed21b474
Deshraj Yadav <deshraj@gatech.edu>
2020-03-23 22:57:54 -0700

    Change command in travis.yml file

--- setup.py
+++ setup.py
@@ -7,27 +7,15 @@ from setuptools.command.test import test as TestCommand
 
 
 class PyTest(TestCommand):
-    user_options = [("pytest-args=", "a", "Arguments to pass into py.test")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        try:
-            from multiprocessing import cpu_count
-
-            self.pytest_args = ["-n", str(cpu_count())]
-        except (ImportError, NotImplementedError):
-            self.pytest_args = ["-n", "1"]
-
     def finalize_options(self):
         TestCommand.finalize_options(self)
-        self.test_args = []
+        self.test_args = ["--cov", ".", "--cov-config", ".coveragerc"]
         self.test_suite = True
 
     def run_tests(self):
         import pytest
 
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
+        sys.exit(pytest.main(self.test_args))
 
 
 PROJECT = "evalai"
@@ -44,7 +32,6 @@ tests_require = [
     "flake8==3.0.4",
     "pytest==3.5.1",
     "pytest-cov==2.5.1",
-    "pytest-xdist",
     "pytest-env==0.6.2",
     "responses==0.9.0",
     "pre-commit==1.14.4",
