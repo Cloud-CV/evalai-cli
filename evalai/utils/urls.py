c68c39e7b53ad251f6ca976116cda3b3bc089cc7
KhalidRmb <khalidriyazgit@gmail.com>
2020-07-29 20:29:59 +0530

    Fix urls

--- evalai/utils/urls.py
+++ evalai/utils/urls.py
@@ -30,6 +30,6 @@ class URLS(Enum):
     )
     download_file = "/api/jobs/submission_files/?bucket={}&key={}"
     phase_details_using_slug = "/api/challenges/phase/{}/"
-    get_annotation_file_presigned_url = "/api/challenges/phases/{}/get_annotation_file_presigned_url/",
-    get_submission_file_presigned_url = "/api/jobs/phases/{}/get_submission_file_presigned_url/",
+    get_annotation_file_presigned_url = "/api/challenges/phases/{}/get_annotation_file_presigned_url/"
+    get_submission_file_presigned_url = "/api/jobs/phases/{}/get_submission_file_presigned_url/"
     send_submission_message = "/api/jobs//phases/{}/send_submission_message/{}/"
