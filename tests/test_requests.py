a18e08e6f2c3ca415ea8b9473547f6084e9d736a
Ram81 <ramramrakhya81@gmail.com>
2020-11-04 23:58:04 +0530

    Add tests for rejecting TandC

--- tests/test_requests.py
+++ tests/test_requests.py
@@ -997,3 +997,17 @@ class TestTeamsSuccess(BaseTestClass):
         result = runner.invoke(challenge, [self.challenge_id, "participate", self.team_id], input="Y")
         response = result.output
         assert response == expected
+    
+    @responses.activate
+    def test_participate_in_a_challenge_decline_terms_and_conditions(self):
+        terms_and_conditions_page_url = "{}{}".format(get_host_url(), URLS.terms_and_conditions_page.value)
+        terms_and_conditions_page_url = terms_and_conditions_page_url.format(self.challenge_id)
+        expected = "Please refer challenge terms and conditions here: {}" \
+        "\n\nBy agreeing to participate in the challenge, you are agreeing to terms and conditions." \
+        "\n\nDo you accept challenge terms and conditions? [y/N]: N".format(terms_and_conditions_page_url) \
+         + "\n\nYou can't participate in the challenge without accepting terms and conditions\n"
+
+        runner = CliRunner()
+        result = runner.invoke(challenge, [self.challenge_id, "participate", self.team_id], input="N")
+        response = result.output
+        assert response == expected
