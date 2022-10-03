698a96e1e4c3543793b43f912a3cd8609e5fb236
Gunjan Chhablani <chhablani.gunjan@gmail.com>
2022-10-03 18:05:31 -0400

    Update endpoint URLs for get challenge

--- evalai/utils/urls.py
+++ evalai/utils/urls.py
@@ -1,12 +1,12 @@
 from enum import Enum
 
-
+GET_ALL_CHALLENGES_URL = "/api/challenges/challenge/time/{}/approved/{}/published/{}"
 class URLS(Enum):
     login = "/api/auth/login"
     get_access_token = "/api/accounts/user/get_auth_token"
-    challenge_list = "/api/challenges/challenge/all"
-    past_challenge_list = "/api/challenges/challenge/past"
-    future_challenge_list = "/api/challenges/challenge/future"
+    challenge_list = GET_ALL_CHALLENGES_URL.format("all", "all", "all")
+    past_challenge_list = GET_ALL_CHALLENGES_URL.format("past", "all", "all")
+    future_challenge_list =GET_ALL_CHALLENGES_URL.format("future", "all", "all")
     challenge_details = "/api/challenges/challenge/{}"
     challenge_phase_details = "/api/challenges/challenge/phase/{}/"
     participant_teams = "/api/participants/participant_team"
