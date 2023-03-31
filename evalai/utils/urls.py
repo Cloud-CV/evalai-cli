e4b6eec49dbd17b9dd1a70028404c64427d09e0f
AyushR1 <ayushrathore12501@gmail.com>
2023-03-31 11:38:35 +0530

    urls.py: Update endpoint to get all challenges
    It was pointing to a wrong endpoint. Fix it with correct one
    
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

--- evalai/utils/urls.py
+++ evalai/utils/urls.py
@@ -4,7 +4,7 @@ from enum import Enum
 class URLS(Enum):
     login = "/api/auth/login"
     get_access_token = "/api/accounts/user/get_auth_token"
-    challenge_list = "/api/challenges/challenge/all"
+    challenge_list = "/api/challenges/challenge/all/all/all"
     past_challenge_list = "/api/challenges/challenge/past"
     future_challenge_list = "/api/challenges/challenge/future"
     challenge_details = "/api/challenges/challenge/{}"
